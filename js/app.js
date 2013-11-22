4a770b114ba006694e8ef24cde1a5d1b3a6907e7
Radford Smith <contact@radsmith.com>
2013-11-21 20:29:26 -0500

    Remove todos

--- js/app.js
+++ js/app.js
@@ -8,78 +8,131 @@ var app = app || {};
   var itemTemplate = _.template($('#item-template').html());
   var statsTemplate = _.template($('#stats-template').html());
 
-  function listenForNewTodos(els, todoApp) {
-    var events = app.helpers.domEvents(els.newTodos, 'keypress');
-
-    events = CSP.filterPull(events, function(event) {
-      return (event.keyCode === ENTER_KEY);
-    });
-
-    events = CSP.mapPull(events, function(event) {
-      return {
-        title: $(event.currentTarget).val(),
-        completed: false
-      };
-    });
-
-    return events;
-  }
-
-  function createTodoApp(newTodos) {
+  function createTodoApp() {
     var todoList = CSP.chan();
     var footer = CSP.chan();
+    var newTodos = CSP.chan();
+    var deleteTodos = CSP.chan();
 
     var remaining = 0;
     var completed = 0;
 
     CSP.go(function*() {
       while (true) {
-        var todo = yield CSP.take(newTodos);
-        remaining++;
+        var selected = yield CSP.alts([newTodos, deleteTodos]);
 
-        yield CSP.put(todoList, {action: 'createItem', item: todo});
-        yield CSP.put(footer, {
-          action: 'updateStats',
-          remaining: remaining,
-          completed: completed
-        });
+        if (newTodos === selected.chan) {
+          remaining++;
+
+          yield CSP.put(todoList, {
+            action: 'createItem',
+            item: selected.value
+          });
+
+          yield CSP.put(footer, {
+            action: 'updateStats',
+            remaining: remaining,
+            completed: completed
+          });
+        } else if (deleteTodos === selected.chan) {
+          remaining--;
+
+          yield CSP.put(todoList, {
+            action: 'deleteItem',
+            el: selected.value
+          });
+
+          yield CSP.put(footer, {
+            action: 'updateStats',
+            remaining: remaining,
+            completed: completed
+          });
+        }
       }
     });
 
     return {
       todoListControl: todoList,
-      footerControl: footer
+      footerControl: footer,
+      newTodos: newTodos,
+      deleteTodos: deleteTodos
     };
   }
 
-  function createTodoListUI(els, control) {
-    var pub = CSP.pub(control, function(val) { return val.action; });
-    var items = CSP.mapPush(CSP.chan(), function(val) { return val.item; });
-    CSP.sub(pub, 'createItem', items);
+  function createTodoListUI(els) {
+    var control = CSP.chan();
+    var newTodos = listenForNewTodos(els.newTodos);
+    var deleteTodos = listenForDeleteTodos(els.todoList);
 
     CSP.go(function*() {
       while (true) {
-        var item = yield CSP.take(items);
-        els.todoList.append(itemTemplate(item));
-        els.newTodos.val('');
+        var val = yield CSP.take(control);
+
+        switch(val.action) {
+          case 'createItem':
+            var item = itemTemplate({
+              completed: false,
+              title: val.item
+            });
+            els.todoList.append(item);
+            els.newTodos.val('');
+            break;
+
+          case 'deleteItem':
+            $(val.el).remove();
+            break;
+        }
       }
     });
+
+    return {
+      control: control,
+      newTodos: newTodos,
+      deleteTodos: deleteTodos
+    };
   }
 
-  function createFooterUI(els, control) {
-    var pub = CSP.pub(control, function(val) { return val.action; });
-    var updates = CSP.mapPush(CSP.chan(), function(val) {
-      return _.pick(val, 'completed', 'remaining');
+  function listenForNewTodos(newTodosEl) {
+    var events = app.helpers.domEvents(newTodosEl, 'keypress');
+
+    events = CSP.filterPull(events, function(event) {
+      return (event.keyCode === ENTER_KEY);
     });
-    CSP.sub(pub, 'updateStats', updates);
+
+    events = CSP.mapPull(events, function(event) {
+      return $(event.currentTarget).val();
+    });
+
+    return events;
+  }
+
+  function listenForDeleteTodos(todoListEl) {
+    var events = app.helpers.domEvents(todoListEl, 'click', '.destroy');
+
+    events = CSP.mapPull(events, function(event) {
+      return $(event.currentTarget).closest('li');
+    });
+
+    return events;
+  }
+
+  function createFooterUI(els) {
+    var control = CSP.chan();
 
     CSP.go(function*() {
       while (true) {
-        var stats = yield CSP.take(updates);
-        els.footer.html('');
-        els.footer.append(statsTemplate(stats));
+        var val = yield CSP.take(control);
+
+        switch(val.action) {
+          case 'updateStats':
+            var stats = _.pick(val, 'completed', 'remaining');
+            els.footer.html('');
+            els.footer.append(statsTemplate(stats));
+        }
       }
     });
+
+    return {control: control};
   }
 
   function showFiltered() {}
@@ -91,11 +144,15 @@ var app = app || {};
       footer: $('#footer')
     };
 
-    var newTodos = listenForNewTodos(els);
-    var todoApp = createTodoApp(newTodos);
+    var todoApp = createTodoApp();
+    var todoListUI = createTodoListUI(els);
+    var footerUI = createFooterUI(els);
 
-    createTodoListUI(els, todoApp.todoListControl);
-    createFooterUI(els, todoApp.footerControl);
+    CSP.pipe(todoListUI.newTodos, todoApp.newTodos);
+    CSP.pipe(todoListUI.deleteTodos, todoApp.deleteTodos);
+
+    CSP.pipe(todoApp.todoListControl, todoListUI.control);
+    CSP.pipe(todoApp.footerControl, footerUI.control);
 
     Router({'/:filter': showFiltered}).init();
   }
--- js/helpers.js
+++ js/helpers.js
@@ -3,21 +3,17 @@ var app = app || {};
 ;(function() {
   'use strict';
 
-  function domEvents(el /* , [ selector,] eventType */) {
-    var selector, eventType;
+  function domEvents(el, eventType, selector) {
     var out = CSP.chan();
 
     function putEvent(event) {
       CSP.putAsync(out, event);
     }
 
-    if (arguments.length === 2) {
-      eventType = arguments[1];
-      $(el).on(eventType, putEvent);
+    if (selector) {
+      $(el).on(eventType, selector, putEvent);
     } else {
-      selector = arguments[1];
-      eventType = arguments[2];
-      $(el).on(selector, eventType, putEvent);
+      $(el).on(eventType, putEvent);
     }
 
     return out;
