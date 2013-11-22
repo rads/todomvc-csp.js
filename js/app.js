179ec17937dcd71d5bababe899088249020fc10f
Radford Smith <contact@radsmith.com>
2013-11-21 20:50:31 -0500

    Merge UI events into one channel

--- js/app.js
+++ js/app.js
@@ -11,64 +11,59 @@ var app = app || {};
   function createTodoApp() {
     var todoList = CSP.chan();
     var footer = CSP.chan();
-    var newTodos = CSP.chan();
-    var deleteTodos = CSP.chan();
+    var uiEvents = CSP.chan();
 
     var remaining = 0;
     var completed = 0;
 
     CSP.go(function*() {
       while (true) {
-        var selected = yield CSP.alts([newTodos, deleteTodos]);
+        var event = yield CSP.take(uiEvents);
 
-        if (newTodos === selected.chan) {
-          remaining++;
+        switch (event.action) {
+          case 'newTodo':
+            remaining++;
+            yield CSP.put(todoList, {action: 'createItem', item: event.value});
+            break;
 
-          yield CSP.put(todoList, {
-            action: 'createItem',
-            item: selected.value
-          });
+          case 'deleteTodo':
+            remaining--;
+            yield CSP.put(todoList, {action: 'deleteItem', el: event.value});
+            break;
 
-          yield CSP.put(footer, {
-            action: 'updateStats',
-            remaining: remaining,
-            completed: completed
-          });
-        } else if (deleteTodos === selected.chan) {
-          remaining--;
-
-          yield CSP.put(todoList, {
-            action: 'deleteItem',
-            el: selected.value
-          });
-
-          yield CSP.put(footer, {
-            action: 'updateStats',
-            remaining: remaining,
-            completed: completed
-          });
+          case 'toggleAll':
+            yield CSP.put(todoList, {action: 'toggleAll'});
+            break;
         }
+
+        yield CSP.put(footer, {
+          action: 'updateStats',
+          remaining: remaining,
+          completed: completed
+        });
       }
     });
 
     return {
       todoListControl: todoList,
       footerControl: footer,
-      newTodos: newTodos,
-      deleteTodos: deleteTodos
+      uiEvents: uiEvents
     };
   }
 
   function createTodoListUI(els) {
     var control = CSP.chan();
-    var newTodos = listenForNewTodos(els.newTodos);
-    var deleteTodos = listenForDeleteTodos(els.todoList);
+    var events = CSP.merge([
+      listenForNewTodos(els.newTodos),
+      listenForDeleteTodos(els.todoList),
+      listenForToggleAllEvents(els.toggleAll)
+    ]);
 
     CSP.go(function*() {
       while (true) {
         var val = yield CSP.take(control);
 
-        switch(val.action) {
+        switch (val.action) {
           case 'createItem':
             var item = itemTemplate({
               completed: false,
@@ -85,11 +80,7 @@ var app = app || {};
       }
     });
 
-    return {
-      control: control,
-      newTodos: newTodos,
-      deleteTodos: deleteTodos
-    };
+    return {control: control, events: events};
   }
 
   function listenForNewTodos(newTodosEl) {
@@ -100,7 +91,10 @@ var app = app || {};
     });
 
     events = CSP.mapPull(events, function(event) {
-      return $(event.currentTarget).val();
+      return {
+        action: 'newTodo',
+        value: $(event.currentTarget).val()
+      };
     });
 
     return events;
@@ -110,7 +104,23 @@ var app = app || {};
     var events = app.helpers.domEvents(todoListEl, 'click', '.destroy');
 
     events = CSP.mapPull(events, function(event) {
-      return $(event.currentTarget).closest('li');
+      return {
+        action: 'deleteTodo',
+        value: $(event.currentTarget).closest('li')
+      };
+    });
+
+    return events;
+  }
+
+  function listenForToggleAllEvents(toggleAllEl) {
+    var events = app.helpers.domEvents(toggleAllEl, 'click');
+
+    events = CSP.mapPull(events, function(event) {
+      return {
+        action: 'toggleAll',
+        value: $(event.currentTarget).val()
+      };
     });
 
     return events;
@@ -123,7 +133,7 @@ var app = app || {};
       while (true) {
         var val = yield CSP.take(control);
 
-        switch(val.action) {
+        switch (val.action) {
           case 'updateStats':
             var stats = _.pick(val, 'completed', 'remaining');
             els.footer.html('');
@@ -141,15 +151,15 @@ var app = app || {};
     var els = {
       newTodos: $('#new-todo'),
       todoList: $('#todo-list'),
-      footer: $('#footer')
+      footer: $('#footer'),
+      toggleAll: $('#toggle-all')
     };
 
     var todoApp = createTodoApp();
     var todoListUI = createTodoListUI(els);
     var footerUI = createFooterUI(els);
 
-    CSP.pipe(todoListUI.newTodos, todoApp.newTodos);
-    CSP.pipe(todoListUI.deleteTodos, todoApp.deleteTodos);
+    CSP.pipe(todoListUI.events, todoApp.uiEvents);
 
     CSP.pipe(todoApp.todoListControl, todoListUI.control);
     CSP.pipe(todoApp.footerControl, footerUI.control);
