7d9d0a5c3320ea5ba89e3ca5af75b99e0e829841
Radford Smith <contact@radsmith.com>
2013-11-21 19:34:14 -0500

    Restructure

--- js/app.js
+++ js/app.js
@@ -8,8 +8,8 @@ var app = app || {};
   var itemTemplate = _.template($('#item-template').html());
   var statsTemplate = _.template($('#stats-template').html());
 
-  function listenForNewTodos(todoApp) {
-    var events = app.helpers.domEvents(todoApp.$newTodos, 'keypress');
+  function listenForNewTodos(els, todoApp) {
+    var events = app.helpers.domEvents(els.newTodos, 'keypress');
 
     events = CSP.filterPull(events, function(event) {
       return (event.keyCode === ENTER_KEY);
@@ -25,13 +25,59 @@ var app = app || {};
     return events;
   }
 
-  function processNewTodos(todoApp, newTodos) {
+  function createTodoApp(newTodos) {
+    var todoList = CSP.chan();
+    var footer = CSP.chan();
+
+    var remaining = 0;
+    var completed = 0;
+
     CSP.go(function*() {
       while (true) {
         var todo = yield CSP.take(newTodos);
-        var item = itemTemplate(todo);
-        todoApp.$todoList.append(item);
-        todoApp.$newTodos.val('');
+        remaining++;
+
+        yield CSP.put(todoList, {action: 'createItem', item: todo});
+        yield CSP.put(footer, {
+          action: 'updateStats',
+          remaining: remaining,
+          completed: completed
+        });
+      }
+    });
+
+    return {
+      todoListControl: todoList,
+      footerControl: footer
+    };
+  }
+
+  function createTodoListUI(els, control) {
+    var pub = CSP.pub(control, function(val) { return val.action; });
+    var items = CSP.mapPush(CSP.chan(), function(val) { return val.item; });
+    CSP.sub(pub, 'createItem', items);
+
+    CSP.go(function*() {
+      while (true) {
+        var item = yield CSP.take(items);
+        els.todoList.append(itemTemplate(item));
+        els.newTodos.val('');
+      }
+    });
+  }
+
+  function createFooterUI(els, control) {
+    var pub = CSP.pub(control, function(val) { return val.action; });
+    var updates = CSP.mapPush(CSP.chan(), function(val) {
+      return _.pick(val, 'completed', 'remaining');
+    });
+    CSP.sub(pub, 'updateStats', updates);
+
+    CSP.go(function*() {
+      while (true) {
+        var stats = yield CSP.take(updates);
+        els.footer.html('');
+        els.footer.append(statsTemplate(stats));
       }
     });
   }
@@ -39,13 +85,17 @@ var app = app || {};
   function showFiltered() {}
 
   function init() {
-    var todoApp = {
-      $newTodos: $('#new-todo'),
-      $todoList: $('#todo-list')
+    var els = {
+      newTodos: $('#new-todo'),
+      todoList: $('#todo-list'),
+      footer: $('#footer')
     };
 
-    var newTodos = listenForNewTodos(todoApp);
-    processNewTodos(todoApp, newTodos);
+    var newTodos = listenForNewTodos(els);
+    var todoApp = createTodoApp(newTodos);
+
+    createTodoListUI(els, todoApp.todoListControl);
+    createFooterUI(els, todoApp.footerControl);
 
     Router({'/:filter': showFiltered}).init();
   }
