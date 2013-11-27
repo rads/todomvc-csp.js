a6065ea796d035a741d2e52161c288f605b67f61
Radford Smith <contact@radsmith.com>
2013-11-27 15:30:57 -0500

    Move UI into a separate file

--- Makefile
+++ Makefile
@@ -1,7 +1,10 @@
-all: build/app.js build/helpers.js
+all: build/app.js build/helpers.js build/ui.js
 
 build/app.js: js/app.js
 	regenerator js/app.js > build/app.js
 
 build/helpers.js: js/helpers.js
 	regenerator js/helpers.js > build/helpers.js
+
+build/ui.js: js/ui.js
+	regenerator js/ui.js > build/ui.js
---
+++ js/ui.js
@@ -0,0 +1,284 @@
+var app = app || {};
+
+;(function() {
+  'use strict';
+
+  var ENTER_KEY = 13;
+
+  var itemTemplate = _.template($('#item-template').html());
+  var statsTemplate = _.template($('#stats-template').html());
+
+  function createTodoListUI(els) {
+    var control = CSP.chan();
+    var events = CSP.merge([
+      listenForNewTodo(els.newTodos),
+      listenForToggleAll(els.toggleAll),
+      listenForClearCompleted(els.todoList)
+    ]);
+
+    var filter = null;
+    var items = {};
+
+    function _createItems(newItems, clearInput) {
+      if (typeof clearInput === 'undefined') clearInput = false;
+
+      createItems(items, newItems, els.todoList, filter, events);
+
+      if (clearInput) {
+        els.toggleAll.prop('checked', false);
+        els.newTodos.val('');
+      }
+    }
+
+    function _deleteItems(ids) {
+      deleteItems(items, ids);
+    }
+
+    function _setItemsStatus(itms, completed) {
+      var item;
+
+      for (var i = 0, len = itms.length; i < len; i++) {
+        item = items[itms[i].id];
+
+        if (!item) {
+          createItems(items, [itms[i]], els.todoList, filter, events);
+        } else if (isIgnoredItem(filter, itms[i])) {
+          deleteItems(items, [itms[i].id]);
+        } else {
+          item.setStatus(completed);
+        }
+      }
+    }
+
+    function _setFilter(itms, filtr) {
+      filter = filtr;
+      deleteItems(items, _.keys(items));
+      createItems(items, itms, els.todoList, filter, events);
+    }
+
+    return {
+      control: control,
+      events: events,
+      createItems: _createItems,
+      deleteItems: _deleteItems,
+      setItemsStatus: _setItemsStatus,
+      setFilter: _setFilter
+    };
+  }
+
+
+  function deleteItems(itemsStore, ids) {
+    var item, i, len;
+    for (i = 0, len = ids.length; i < len; i++) {
+      item = itemsStore[ids[i]];
+      if (item) {
+        delete itemsStore[ids[i]];
+        $(item.el).remove();
+      }
+    }
+  }
+
+  function isIgnoredItem(filter, item) {
+    return (filter === 'completed' && !item.completed) ||
+           (filter === 'active' && item.completed);
+  }
+
+  function createItems(itemsStore, newItems, todoListEl, filter, events) {
+    var i, len, item;
+    for (i = 0, len = newItems.length; i < len; i++) {
+      if (isIgnoredItem(filter, newItems[i])) continue;
+
+      item = itemsStore[newItems[i].id] = createTodoItemUI(newItems[i]);
+      CSP.pipe(item.events, events);
+      todoListEl.prepend(item.el);
+    }
+  }
+
+  function listenForNewTodo(newTodosEl) {
+    var events = app.helpers.domEvents(newTodosEl, 'keypress');
+
+    events = CSP.filterPull(events, function(event) {
+      return (event.keyCode === ENTER_KEY);
+    });
+
+    return CSP.mapPull(events, function(event) {
+      return {
+        action: 'newTodo',
+        title: $(event.currentTarget).val()
+      };
+    });
+  }
+
+  function listenForToggleAll(toggleAllEl) {
+    var events = app.helpers.domEvents(toggleAllEl, 'click');
+
+    return CSP.mapPull(events, function(event) {
+      return {
+        action: 'toggleAll',
+        completed: $(event.currentTarget).prop('checked')
+      };
+    });
+  }
+
+  function createTodoItemUI(item) {
+    var id = item.id;
+    var el = $(itemTemplate(item));
+    var done = CSP.chan();
+
+    var control = CSP.chan();
+    var events = CSP.merge([
+      listenForToggleOne(el, id),
+      listenForDeleteTodo(el, id)
+    ]);
+
+    if (item.completed) $(el).addClass('completed');
+
+    var editing = editingEvents(el);
+    var isEditing = false;
+
+    CSP.goLoop(function*() {
+      var result = yield CSP.alts([editing, done]);
+
+      if (result.val) {
+        _startEditing();
+        isEditing = true;
+      } else {
+        if (!isEditing) return;
+        _stopEditing();
+        isEditing = false;
+      }
+    });
+
+    function _delete() {
+      el.remove();
+    }
+
+    function _setStatus(completed) {
+      el.find('.toggle').prop('checked', completed);
+      el.toggleClass('completed', completed);
+    }
+
+    function _startEditing() {
+      el.addClass('editing');
+      el.find('.edit').select();
+    }
+
+    function _stopEditing() {
+      var title = el.find('.edit').val();
+      el.removeClass('editing');
+      el.find('label').text(title);
+
+      CSP.putAsync(events, {
+        action: 'updateTodo',
+        id: id,
+        title: title
+      });
+    }
+
+    return {
+      control: control,
+      events: events,
+      el: el,
+      delete: _delete,
+      setStatus: _setStatus
+    };
+  }
+
+  function listenForToggleOne(todoItemEl, id) {
+    var events = app.helpers.domEvents(todoItemEl, 'click', '.toggle');
+
+    return CSP.mapPull(events, function(event) {
+      return {action: 'toggleOne', id: id};
+    });
+  }
+
+  function listenForDeleteTodo(todoItemEl, id) {
+    var events = app.helpers.domEvents(todoItemEl, 'click', '.destroy');
+
+    return CSP.mapPull(events, function(event) {
+      return {action: 'deleteTodo', id: id};
+    });
+  }
+
+  function constantly(val) {
+    return function() {
+      return val;
+    };
+  }
+
+  function editingEvents(todoItemEl) {
+    var start = app.helpers.domEvents(todoItemEl, 'dblclick');
+
+    var bodyClicks = app.helpers.domEvents($('html'), 'click');
+    bodyClicks = CSP.removePull(bodyClicks, function(event) {
+      return $(event.target).closest(todoItemEl).length;
+    });
+
+    var enterPresses = app.helpers.domEvents(todoItemEl, 'keypress', '.edit');
+    enterPresses = CSP.filterPull(enterPresses, function(event) {
+      return (event.keyCode === ENTER_KEY);
+    });
+
+    var stop = CSP.merge([bodyClicks, enterPresses]);
+
+    var out = CSP.merge([
+      CSP.mapPull(start, constantly(true)),
+      CSP.mapPull(stop, constantly(false))
+    ]);
+
+    return CSP.unique(out);
+  }
+
+  function createFooterUI(els) {
+    var control = CSP.chan();
+    var events = CSP.merge([
+      listenForClearCompleted(els.footer)
+    ]);
+
+    var filter = null;
+
+    function _updateStats(stats) {
+      els.footer.
+        show().
+        html('').
+        append(statsTemplate(stats));
+      setSelectedFilterLink(els.footer, filter);
+    }
+
+    function _setFilter(filtr) {
+      filter = filtr;
+      setSelectedFilterLink(els.footer, filter);
+    }
+
+    function _hide() {
+      return els.footer.hide();
+    }
+
+    return {
+      control: control,
+      events: events,
+      updateStats: _updateStats,
+      setFilter: _setFilter,
+      hide: _hide
+    };
+  }
+
+  function setSelectedFilterLink(footerEl, filter) {
+    var filtersEl = footerEl.find('#filters');
+    filtersEl.find('a').removeClass('selected');
+    filtersEl.find('a[href="#/' + (filter || '') + '"]').addClass('selected');
+  }
+
+  function listenForClearCompleted(footerEl) {
+    var events = app.helpers.domEvents(footerEl, 'click', '#clear-completed');
+
+    return CSP.mapPull(events, function(event) {
+      return {action: 'clearCompleted'};
+    });
+  }
+
+  app.ui = {
+    createTodoListUI: createTodoListUI,
+    createFooterUI: createFooterUI
+  };
+})();
